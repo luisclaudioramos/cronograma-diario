acde4e3f90a61137a816e54c2c254fdf6c1bc9d4
Luis Cláudio da Silva Ramos <luisclaudioramos@gmail.com>
2023-10-02 18:24:06 -0300

    feat(Schedule): Optimize the schedule data to show on report.

--- daily-schedule/src/app/app.component.ts
+++ daily-schedule/src/app/app.component.ts
@@ -22,33 +22,37 @@ export class AppComponent {
   ];
 
   public tasks: TasksInterface[] = [
-    {id: 1, category_id: 6, name: 'Café da manhã', date_of_completion: '01/10/2023', hour_of_completion: '08:00:00', start_date: '30/09/2023', end_date: '', status: 'Em andamento'},
-    {id: 1, category_id: 3, name: 'Iniciar o trabalho', date_of_completion: '01/10/2023', hour_of_completion: '09:00:00', start_date: '30/09/2023', end_date: '', status: 'Em andamento'},
-    {id: 1, category_id: 3, name: 'Iniciar o trabalho', date_of_completion: '01/10/2023', hour_of_completion: '10:00:00', start_date: '', end_date: '', status: 'Em andamento'},
-    {id: 1, category_id: 3, name: 'Iniciar o trabalho', date_of_completion: '01/10/2023', hour_of_completion: '11:00:00', start_date: '', end_date: '', status: 'Em andamento'},
-    {id: 1, category_id: 6, name: 'Almoçar', date_of_completion: '01/10/2023', hour_of_completion: '12:00:00', start_date: '', end_date: '', status: 'Em andamento'},
-    {id: 1, category_id: 3, name: 'Iniciar o trabalho', date_of_completion: '01/10/2023', hour_of_completion: '13:00:00', start_date: '', end_date: '', status: 'Em andamento'},
-    {id: 1, category_id: 3, name: 'Iniciar o trabalho', date_of_completion: '01/10/2023', hour_of_completion: '14:00:00', start_date: '', end_date: '', status: 'Em andamento'},
-    {id: 1, category_id: 3, name: 'Iniciar o trabalho', date_of_completion: '01/10/2023', hour_of_completion: '15:00:00', start_date: '', end_date: '', status: 'Em andamento'},
-    {id: 1, category_id: 3, name: 'Iniciar o trabalho', date_of_completion: '01/10/2023', hour_of_completion: '16:00:00', start_date: '', end_date: '', status: 'Em andamento'},
-    {id: 1, category_id: 3, name: 'Iniciar o trabalho', date_of_completion: '01/10/2023', hour_of_completion: '17:00:00', start_date: '', end_date: '', status: 'Em andamento'},
-    {id: 1, category_id: 1, name: 'Passar um tempo com ele todo dia.', date_of_completion: '01/10/2023', hour_of_completion: '18:00:00', start_date: '', end_date: '', status: 'Em andamento' },
-    {id: 1, category_id: 1, name: 'Passar um tempo com ele todo dia.', date_of_completion: '01/10/2023', hour_of_completion: '19:00:00', start_date: '', end_date: '', status: 'Em andamento' },
-    {id: 1, category_id: 5, name: 'Ir a academia', date_of_completion: '01/10/2023', hour_of_completion: '20:00:00', start_date: '', end_date: '', status: 'Em andamento'},
-    {id: 1, category_id: 5, name: 'Ir a academia', date_of_completion: '01/10/2023', hour_of_completion: '21:00:00', start_date: '', end_date: '', status: 'Em andamento'},
-    {id: 1, category_id: 6, name: 'Jantar', date_of_completion: '01/10/2023', hour_of_completion: '22:00:00', start_date: '', end_date: '', status: 'Em andamento'},
-    {id: 1, category_id: 6, name: 'Jantar', date_of_completion: '02/10/2023', hour_of_completion: '22:00:00', start_date: '', end_date: '', status: 'Em andamento'}
+    {id: 1, category_id: 6, name: 'Café da manhã', date_of_completion: '02/10/2023', hour_of_completion: '08:00:00', start_date: '30/09/2023', end_date: '', status: 'Em andamento'},
+    {id: 1, category_id: 3, name: 'Iniciar o trabalho', date_of_completion: '02/10/2023', hour_of_completion: '09:00:00', start_date: '30/09/2023', end_date: '', status: 'Em andamento'},
+    {id: 1, category_id: 3, name: 'Iniciar o trabalho', date_of_completion: '02/10/2023', hour_of_completion: '10:00:00', start_date: '', end_date: '', status: 'Em andamento'},
+    {id: 1, category_id: 3, name: 'Iniciar o trabalho', date_of_completion: '02/10/2023', hour_of_completion: '11:00:00', start_date: '', end_date: '', status: 'Em andamento'},
+    {id: 1, category_id: 6, name: 'Almoçar', date_of_completion: '02/10/2023', hour_of_completion: '12:00:00', start_date: '', end_date: '', status: 'Em andamento'},
+    {id: 1, category_id: 3, name: 'Iniciar o trabalho', date_of_completion: '02/10/2023', hour_of_completion: '13:00:00', start_date: '', end_date: '', status: 'Em andamento'},
+    {id: 1, category_id: 3, name: 'Iniciar o trabalho', date_of_completion: '02/10/2023', hour_of_completion: '14:00:00', start_date: '', end_date: '', status: 'Em andamento'},
+    {id: 1, category_id: 3, name: 'Iniciar o trabalho', date_of_completion: '02/10/2023', hour_of_completion: '15:00:00', start_date: '', end_date: '', status: 'Em andamento'},
+    {id: 1, category_id: 3, name: 'Iniciar o trabalho', date_of_completion: '02/10/2023', hour_of_completion: '16:00:00', start_date: '', end_date: '', status: 'Em andamento'},
+    {id: 1, category_id: 3, name: 'Iniciar o trabalho', date_of_completion: '02/10/2023', hour_of_completion: '17:00:00', start_date: '', end_date: '', status: 'Em andamento'},
+    {id: 1, category_id: 1, name: 'Passar um tempo com ele todo dia.', date_of_completion: '02/10/2023', hour_of_completion: '18:00:00', start_date: '', end_date: '', status: 'Em andamento' },
+    {id: 1, category_id: 1, name: 'Passar um tempo com ele todo dia.', date_of_completion: '02/10/2023', hour_of_completion: '19:00:00', start_date: '', end_date: '', status: 'Em andamento' },
+    {id: 1, category_id: 5, name: 'Ir a academia', date_of_completion: '02/10/2023', hour_of_completion: '20:00:00', start_date: '', end_date: '', status: 'Em andamento'},
+    {id: 1, category_id: 5, name: 'Ir a academia', date_of_completion: '02/10/2023', hour_of_completion: '21:00:00', start_date: '', end_date: '', status: 'Em andamento'},
+    {id: 1, category_id: 6, name: 'Jantar', date_of_completion: '02/10/2023', hour_of_completion: '22:00:00', start_date: '', end_date: '', status: 'Em andamento'},
+
+    {id: 1, category_id: 6, name: 'Café da manhã', date_of_completion: '03/10/2023', hour_of_completion: '08:00:00', start_date: '', end_date: '', status: 'Em andamento'}
   ];
 
   public date: Date = new Date();
-  public days: number = 10;
-  public hours: number = 24;
+  public number_of_days: number = 7;
+  public days: string[] = [];
+  public schedules: any[] = [];
 
   constructor() {
+    this.getDays();
+    this.getData();
   }
 
-  public getDays(): string[] {
-    const numberOfDays = this.days;
+  public getDays(): void {
+    const numberOfDays = this.number_of_days;
 
     const daysArray = [];
     for (let i = 0; i < numberOfDays; i++) {
@@ -57,32 +61,11 @@ export class AppComponent {
       daysArray.push(formattedDate);
     }
 
-    return daysArray;
-  }
-
-  public getDayHours(): string[] {
-    const total_hours = this.hours;
-    const hours: string[] = [];
-
-    for (let i = 0; i < total_hours + 1; i++) {
-      hours.push(i < 10 ? '0' + i + ':00:00' : i + ':00:00')
-    }
-    return hours;
+    this.days = daysArray;
   }
 
   public getData() {
-
+    const days: string[] = this.days;
+    this.schedules = this.tasks.map(task => ({ task, days }));
   }
-
-  public getTaskMatchWithHour(hour: string): TasksInterface {
-    return this.tasks.filter(i => i.hour_of_completion === hour)[0];
-  }
-
-  public isMatchDayAndHour(task: TasksInterface, day: string, hour: string): boolean {
-    if (task && task.date_of_completion === day && task.hour_of_completion === hour) {
-      return true;
-    }
-    return false
-  }
-
 }
